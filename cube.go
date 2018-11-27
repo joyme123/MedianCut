d33c05696b39c01a4981b914554dce845a5fc5aa
joyme <jiangpengfei12@gmail.com>
2018-11-27 22:08:21 +0800

    more

--- cube.go
+++ cube.go
@@ -1,12 +1,12 @@
 package main
 
 type ColorCube struct {
-	Lower int // 盒子的低位指针
-	Upper int // 盒子的高位指针
-	count int // 盒子中的颜色数量
-	level int // 盒子切割的深度
+	Lower int16 // 盒子的低位指针
+	Upper int16 // 盒子的高位指针
+	Count int   // 盒子中的颜色数量
+	Level int   // 盒子切割的深度
 
-	rmin, rmax, gmin, gmax, bmin, bmax byte // RGB值的范围
+	Rmin, Rmax, Gmin, Gmax, Bmin, Bmax byte // RGB值的范围
 }
 
 // Shrink 方法会将盒子收缩
@@ -17,4 +17,46 @@ func (cube *ColorCube) Shrink() {
 // GetColor 用来获取盒子代表的颜色
 func (cube *ColorCube) GetColor() {
 
+}
+
+type HistList struct {
+	histPtr []int16
+}
+
+func (hist *HistList) Init(histPtr []int16, longdim int) {
+	hist.histPtr = histPtr
+}
+
+func (hist HistList) Len() int {
+	return len(hist.histPtr)
+}
+
+func (hist HistList) Less(i, j int) bool {
+	colorA := hist.histPtr[i]
+	colorB := hist.histPtr[j]
+
+	var c1 byte
+	var c2 byte
+
+	switch longdim {
+
+	case 0:
+		c1 = RED(colorA)
+		c2 = RED(colorB)
+		break
+	case 1:
+		c1 = GREEN(colorA)
+		c2 = GREEN(colorB)
+		break
+	case 2:
+		c1 = BLUE(colorA)
+		c2 = BLUE(colorB)
+		break
+	}
+	return c1-c2 < 0
+
+}
+
+func (hist HistList) Swap(i, j int) {
+	hist.histPtr[i], hist.histPtr[j] = hist.histPtr[j], hist.histPtr[i]
 }
--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 const HSIZE = 32768
+const MAXCOLORS = 256
 
 var histPtr [HSIZE]int16
+var cubeList [MAXCOLORS]ColorCube
+var longdim int
 
 // MedianCut 用来切割ColorCude, hist是图片中颜色的直方图
 func MedianCut(hist []int16, colorMap [][3]byte, maxCubes int) int {
@@ -18,7 +22,7 @@ func MedianCut(hist []int16, colorMap [][3]byte, maxCubes int) int {
 	var cube, cubeA, cubeB ColorCube
 
 	ncubes = 0
-	cube.count = 0
+	cube.Count = 0
 
 	i = 0
 	color = 0
@@ -26,11 +30,86 @@ func MedianCut(hist []int16, colorMap [][3]byte, maxCubes int) int {
 	for ; i < HSIZE-1; i++ {
 		if hist[i] != 0 {
 			histPtr[color] = i
-			color += 1
-			cube.count = cube.count + (int)(hist[i])
+			color++
+			cube.Count = cube.Count + (int)(hist[i])
 		}
 	}
 
+	cube.Lower = 0
+	cube.Upper = color - 1
+	cube.Level = 0
+
+	cube.Shrink()
+	cubeList[ncubes] = cube
+	ncubes++
+
+	for ncubes < maxCubes {
+		level = 255
+		splitpos = -1
+
+		for k = 0; k < ncubes-1; k++ {
+			if cubeList[k].Lower == cubeList[k].Upper {
+
+			} else if cubeList[k].Level < level {
+				level = cubeList[k].Level
+				splitpos = k
+			}
+		}
+
+		if splitpos == -1 {
+			break
+		}
+
+		cube = cubeList[splitpos]
+		lr = cube.Rmax - cube.Rmin
+		lg = cube.Gmax - cube.Gmin
+		lb = cube.Bmax - cube.Bmin
+
+		if lr >= lg && lr >= lb {
+			longdim = 0
+		}
+
+		if lg >= lr && lg >= lb {
+			longdim = 1
+		}
+
+		if lb >= lr && lb >= lg {
+			longdim = 2
+		}
+
+		var histList HistList
+		histList.Init(histPtr[cube.Lower:cube.Upper], longdim)
+
+		sort.Sort(histList)
+
+		count = 0
+		for i = cube.Lower; i <= cube.Upper-1; i++ {
+			if count >= cube.Count/2 {
+				break
+			}
+			color = histPtr[i]
+			count = count + (int)(hist[color])
+		}
+
+		median = i
+
+		cubeA := cube
+		cubeA.Upper = median - 1
+		cubeA.Count = count
+		cubeA.Level = cube.Level + 1
+		cubeA.Shrink()
+		cubeList[splitpos] = cubeA
+
+		cubeB := cube
+		cubeB.Lower = median
+		cubeB.Count = cube.Count - count
+		cubeB.Level = cube.Level + 1
+		cubeB.Shrink()
+		cubeList[ncubes] = cubeB
+		ncubes++
+	}
+
+	// 得到了足够的切割后的cube，现在计算所有方块的颜色,做颜色的映射
 }
 
 func main() {
